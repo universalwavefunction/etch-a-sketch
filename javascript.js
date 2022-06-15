7d51f204017b8efb73eac1b3e466e8390e925c21
helan <helanmah@gmail.com>
2022-06-14 23:35:46 -0700

    planned layout of project

---
+++ javascript.js
@@ -0,0 +1,8 @@
+//set width and height = to x
+//loop through width / by size of blocks
+//create function that takes click position and
+//maps it to relevant div, then changes that divs color
+
+const grid = document.querySelector('#grid-container');
+
+grid.style.cssText =
